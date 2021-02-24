b124d6b79d00983a9cbece687b8b756c1deb9a71
SNarboni <sebastien.narboni@hotmail.fr>
2021-02-24 15:09:34 +0100

    liste finit

---
+++ jour3/liste/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View,Image } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import List from "./components/List";
+
+const App = () => {
+  const [tab, setTab] = useState("");
+
+  const searchCountry = () => {
+    fetch("http://restcountries.eu/rest/v2/all")
+      .then((response) => response.json())
+      .then((result) => {
+        setTab(result);
+        console.log(result[0].flag);
+      });
+  };
+
+  useEffect(() => {
+    searchCountry();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={tab}
+        renderItem={(element) => {
+          return (<List name={element.item.name} capital={element.item.capital} flag={element.item.flag}></List>);
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 60,
+  },
+  listContainer:{
+    borderWidth: 2,
+    display:1,
+    flexDirection:"column",
+    alignItems: "center",
+    paddingVertical:15,
+    marginVertical:20,
+  },
+  listText:{
+    fontSize:20,
+  }
+});
+export default App;
---
+++ jour3/liste/components/List.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+ const List = ({name, capital, flag}) => {
+
+
+        return (
+            <View style={styles.listContainer}>
+                <Image source={{uri:flag}} style={{height:150, width:225}}/>
+                <Text style={styles.listText}>Pays : {name}</Text>
+                <Text style={styles.listText}>Capital : {capital}</Text>
+            </View>
+        )
+}
+
+const styles = StyleSheet.create({
+    listContainer:{
+      borderWidth: 2,
+      display:1,
+      flexDirection:"column",
+      alignItems: "center",
+      paddingVertical:15,
+      marginVertical:20,
+    },
+    listText:{
+      fontSize:20,
+    }
+  });
+export default List;
